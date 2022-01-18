c0d492376efbb031b3a27a9662ba61dffafc8b6a
unfussrgarlic <vishal_sreenivasan@srmuniv.edu.in>
2022-01-18 07:49:04 +0100

    Update boxplot

--- Task2/main.py
+++ Task2/main.py
@@ -1,35 +1,71 @@
+import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from ss import SemiSupervised
 
-if __name__ == "__main__":
-    df = pd.read_csv("data/creditcard.csv")
-    df = df.drop(["Time", "Amount"], axis=1)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data", help = "Folder of creditcard.csv")
+    parser.add_argument("res" ,help = "Folder where the plots will be saved")
 
-    ss = SemiSupervised(df, neighbors=3)
+    return parser.parse_args()
+
+def plot(args, scores: list, type: str) -> None:
+    titles = ["Baseline model (KNN): Epochs vs F1 score", 
+              "Semi-supervised model (LinearPropogation): Epochs vs F1 score", 
+              "Baseline model (KNN) training with Semi-supervised model (LinearPropogation) labels: Epochs vs F1 score"]
+
+    if type == "line":
+        for i in range(len(scores)):
+            fig, ax = plt.subplots(figsize=(15, 8))
+            ax.plot(scores[i], c="r")
+            ax.set_title(titles[i])
+            ax.set_xlabel("Epochs")
+            ax.set_ylabel("F1 score")
+
+            fig.savefig(f"{args.res}/q{i}.png", bbox_inches="tight")
+    
+    elif type == "box":
+        labels = ["Knn", "SS", "Knn+SS"]
+        fig, ax = plt.subplots(figsize=(16, 15))
+
+        plt.subplots_adjust(left=0.075, right=0.95, top=0.9, bottom=0.25)
+
+        bp = ax.boxplot(x = scores, vert = True, whis = 0.5, labels = labels, manage_ticks = True, showmeans = True)
+        plt.setp(bp['boxes'], color='red')
+        plt.setp(bp['whiskers'], color='black')
+        plt.setp(bp["means"], marker = "o")
+        plt.setp(bp['fliers'], marker='.')
+
+        ax.set_ylabel("F score")
+        ax.set_xlabel("Models")
+        ax.grid(False)
+
+        fig.savefig(f"{args.res}/plot.png", bbox_inches = "tight")
+
+def main():
+    args = parse_args()
+    df = pd.read_csv(f"{args.data}/creditcard.csv")
+    df = df.drop(["Time", "Amount"], axis = 1)
+
+    ss = SemiSupervised(df, neighbors = 3)
 
     s1, s2, s3 = ss.run()
 
     print("================\n")
-
+    
     print(f"Average q2 score: {np.mean(s1)}")
     print(f"Average q3 score: {np.mean(s2)}")
     print(f"Average q4 score: {np.mean(s3)}")
 
-    scores = [s1, s2, s3]
-    titles = [
-        "Baseline model (KNN): Epochs vs F1 score",
-        "Semi-supervised model (LinearPropogation): Epochs vs F1 score",
-        "Baseline model (KNN) training with Semi-supervised model (LinearPropogation) labels: Epochs vs F1 score",
-    ]
+    print("Plotting and Saving the results...")
 
-    for i in range(len(scores)):
-        fig, ax = plt.subplots(figsize=(15, 8))
-        ax.plot(scores[i], c="r")
-        ax.set_title(titles[i])
-        ax.set_xlabel("Epochs")
-        ax.set_ylabel("F1 score")
+    plots = ["line", "box"]
 
-        fig.savefig(f"results/q{i}.png", bbox_inches="tight")
+    for i in range(len(plots)):
+        plot(args, [s1, s2, s3], plots[i])
+    
+if __name__ == "__main__":
+    main()
