d7babdee799dd10d13d43726351fa247b5d60d6f
jorik <39156782+AI12018@users.noreply.github.com>
2022-01-23 22:15:07 +0100

    max region function

--- Task1/find_max_region.py
+++ Task1/find_max_region.py
@@ -1,23 +1,30 @@
 import numpy as np
 import math
 
-results_array = np.genfromtxt('my_file.csv', delimiter=',')
+results_array = np.genfromtxt('data/results/cats/sift_acc_svm.csv', delimiter=',')
 
+print(np.shape(results_array))
 n_rows = results_array.shape[0]
 n_cols = results_array.shape[1]
 
-window_size = 6
+window_size = 1
 
 max_value = 0
 max_averages = []
+max_region = []
+
+print(results_array[1:2, 1:2])
 
 for row in range(((n_rows + 1) - window_size)):
     for col in range(((n_cols + 1) - window_size)):
-        average = np.average(results_array[row:(row + window_size)][col:(col + window_size)])
-        if math.isclose(average, max_value, rel_tol=1e-4):
+        average = np.average(results_array[row:(row + window_size), col:(col + window_size)])
+        if math.isclose(average, max_value, rel_tol=1e-3):
             max_averages.append(average)
-        elif np.average(results_array[row:(row + window_size)][col:(col + window_size)]) > max_value:
+            max_region.append([row + 1, col + 1])
+        elif average > max_value:
             max_value = average
             max_averages = [average]
+            max_region = [[row + 1, col + 1]]
 
-
+print("Max averages regions are ", max_averages)
+print("Max region rows and cols are ", max_region)
